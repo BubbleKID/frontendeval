c679d8c0385d878422eb2c1b17aaf3c860f46b92
Mark <fqchenxin@gmail.com>
2023-09-13 20:25:49 +1000

    add array question

---
+++ Array.prototype.map/index.js
@@ -0,0 +1,22 @@
+/*
+* https://frontendeval.com/questions/array-prototype-map
+*
+* Implement a barebones Array.prototype.map function
+*/
+
+function map(array, callback) {
+    let result = [];
+    
+    if (!Array.isArray(array)) return 'Invalid array';
+    if (typeof callback !== 'function') return 'Invalid callback';
+    
+    for (let i = 0; i < array.length; i++) {
+        result.push(callback(array[i]));
+    }
+
+    return result;
+}
+
+const mappedArray = map(null, (x) => x * 2);
+console.log(mappedArray);
+// [2, 4, 6, 8, 10];
